63a46d81519f33060f42e98f09e3421c491672c8
James Wick <jmwick2@hotmail.com>
2017-09-01 23:50:32 -0700

    heroku

--- server.js
+++ server.js
@@ -15,15 +15,12 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-// var exphbs = require("express-handlebars");
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
 
-// mongoose.connect("mongodb://heroku_z2rz24m6:sj29b4n8g08g47q21gp5sijj9e@ds151222.mlab.com:51222/heroku_z2rz24m6");
+mongoose.connect("mongodb://heroku_ggtmz2k5:6hg8i2854cm45chmmf1s3aougt@ds121674.mlab.com:21674/heroku_ggtmz2k5");
 
-mongoose.connect("mongodb://localhost/nytreact", {
-  useMongoClient: true
-});
+// mongoose.connect("mongodb://localhost/nytreact", {
+//   useMongoClient: true
+// });
 
 var db = mongoose.connection;
 
@@ -35,18 +32,10 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-//handles getting and deleting articles from Mongo
-// const news = require("./controllers/news-controller.js");
-//handles the scrape
-// const scrape = require("./controllers/scrape-controller.js");
-//handles getting, adding, and deleting notes
-// const notes = require("./controllers/notes-controller.js");
-//handles saving and unsaving articles
+
 const saved = require("./controllers/saved-controller.js");
 
-// app.use("/", news);
-// app.use("/scrape", scrape);
-// app.use("/notes", notes);
+
 app.use("api/saved", saved);
 
 
